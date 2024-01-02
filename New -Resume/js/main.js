6af83b8c93debaebb2797127bee47ad55b8d9dac
six love <132566900+aaruanbu@users.noreply.github.com>
2024-01-02 12:11:56 +0530

    Add files via upload

---
+++ New -Resume/js/main.js
@@ -0,0 +1,29 @@
+function shownav(){
+	var ele=document.getElementById('sho')
+	ele.classList.toggle("dnon")
+	console.log("runn")
+}
+ function dark(){
+ 	// console.log("hello")
+ 	let bddark=document.getElementById('boddark')
+ 	bddark.classList.toggle("boddark")
+ 	
+
+ }
+ function sun(){
+ 	console.log("su")
+sunns=document.getElementById("sunns")
+ 	sunns.classList.toggle("sunns")
+ 	}
+ 	
+ 	function moon(){
+ 		console.log("mo")
+ 		mons=document.getElementById("moons")
+ 		mons.classList.toggle("moons")
+ 	}
+ 	// sunns.classList.toggle("sunns")
+
+ 	// function submi(){
+ 	// 	console.log("submit")
+ 	// }
+
